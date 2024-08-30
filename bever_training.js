10c7d985885d8fb6eadaffb459746ab4eecab037
Kost333 <levonkostanyan2021@gmail.com>
2024-08-30 22:51:37 +0400

    set currency from price list, autofill and hide fields, set price per unit.

--- bever_training.js
+++ bever_training.js
@@ -55,6 +55,34 @@ function enableAllFields(executionContext) {
     });
 }
 
+async function setCurrencyFromPriceList(executionContext) {
+    const Form = executionContext.getFormContext();
+    const priceListLookup = Form.getAttribute("cr62c_fk_price_list").getValue();
+
+    if (priceListLookup == null) {
+        Form.getControl("TransactionCurrencyId").setDisabled(true);
+        return;
+    }
+
+    const priceListId = priceListLookup[0].id;
+
+    try {
+        const priceListRecord = await Xrm.WebApi.retrieveMultipleRecords("pricelist",
+            `?$select=TransactionCurrencyId&$filter=cr62c_fk_price_list eq ${priceListId}&$expand=transactioncurrencyid($select=transactioncurrencyid)`);
+
+        if (priceListRecord.entities.length > 0) {
+            const currencyId = priceListRecord.entities[0].transactioncurrencyid.transactioncurrencyid;
+
+            Form.getAttribute("TransactionCurrencyId").setValue([{ id: currencyId, entityType: "TransactionCurrency" }]);
+            Form.getControl("TransactionCurrencyId").setDisabled(true);
+        }
+    } catch (error) {
+        console.error("Error retrieving currency from Price List: ", error);
+        Xrm.Navigation.openAlertDialog({ text: "An error occurred while retrieving currency from Price List." });
+    }
+}
+
+
 async function autofillCurrencyFromPriceList(executionContext) {
     const Form = executionContext.getFormContext();
     const priceList = Form.getAttribute("cr62c_fk_price_list").getValue();
@@ -63,25 +91,75 @@ async function autofillCurrencyFromPriceList(executionContext) {
         const priceListId = priceList[0].id;
 
         try {
-            const priceListRecord = await Xrm.WebApi.retrieveRecord("cr62c_price_list", priceListId, "?$select=transactioncurrencyid");
+            const priceListRecord = await Xrm.WebApi.retrieveRecord("cr62c_price_list", priceListId, "?$select=TransactionCurrencyId");
 
             if (priceListRecord.transactioncurrencyid) {
                 const currencyId = priceListRecord.transactioncurrencyid.transactioncurrencyid;
                 const currencyName = priceListRecord.transactioncurrencyid.name;
 
-                Form.getAttribute("transactioncurrencyid").setValue([{
+                Form.getAttribute("TransactionCurrencyId").setValue([{
                     id: currencyId,
                     name: currencyName,
                     entityType: "transactioncurrency"
                 }]);
 
-                Form.getControl("transactioncurrencyid").setDisabled(true);
+                Form.getControl("TransactionCurrencyId").setDisabled(true);
             }
         } catch (error) {
             console.error("Error retrieving currency from Price List:", error);
         }
     }
 }
+
+function autofillAndHideFields(executionContext) {
+    const Form = executionContext.getFormContext();
+    const product = Form.getAttribute("cr62c_product").getValue();
+
+    if (product != null) {
+        const productName = product[0].name;
+        Form.getAttribute("cr62c_slot_name").setValue(productName);
+    }
+
+    Form.getControl("cr62c_slot_name").setVisible(false);
+    Form.getControl("OwnerId").setVisible(false);
+}
+
+    async function setPricePerUnit(executionContext) {
+    const Form = executionContext.getFormContext();
+    const productLookup = Form.getAttribute("cr62c_Product").getValue();
+    const priceListLookup = Form.getAttribute("cr62c_fk_price_list").getValue();
+
+    if (productLookup == null || priceListLookup == null) {
+        return;
+    }
+
+    const productId = productLookup[0].id;
+    const priceListId = priceListLookup[0].id;
+
+    try {
+        const priceListItemRecords = await Xrm.WebApi.retrieveMultipleRecords("pricelistitem",
+            `?$filter=_cr62c_fk_price_list_value eq ${priceListId} and _cr62c_Product_value eq ${productId}`);
+
+        let pricePerUnit = null;
+
+        if (priceListItemRecords.entities.length > 0) {
+            pricePerUnit = priceListItemRecords.entities[0].priceperunit;
+        } else {
+            const productRecord = await Xrm.WebApi.retrieveRecord("product", productId, "?$select=cr62c_mon_price_list");
+            pricePerUnit = productRecord.defaultpriceperunit;
+        }
+
+        if (pricePerUnit !== null) {
+            Form.getAttribute("mon_price_per_unit").setValue(pricePerUnit);
+            Form.getControl("mon_price_per_unit").setDisabled(true);
+        }
+
+    } catch (error) {
+        console.error("Error retrieving Price Per Unit: ", error);
+        Xrm.Navigation.openAlertDialog({ text: "An error occurred while retrieving Price Per Unit." });
+    }
+}
+
 
 async function autofillPricePerUnit(executionContext) {
     const Form = executionContext.getFormContext();
@@ -93,14 +171,14 @@ async function autofillPricePerUnit(executionContext) {
         const productId = product[0].id;
 
         try {
-            const query = `?$filter=_productid_value eq ${productId} and _pricelistid_value eq ${priceListId} &$select=priceperunit`;
+            const query = `?$filter=_cr62c_fk_product_value eq ${productId} and _cr62c_fk_price_list_value eq ${priceListId} &$select=mon_price_per_unit`;
             const priceListItemRecords = await Xrm.WebApi.retrieveMultipleRecords("pricelistitem", query);
 
             if (priceListItemRecords.entities.length > 0) {
                 const pricePerUnit = priceListItemRecords.entities[0].priceperunit;
                 Form.getAttribute("cr62c_mon_price_per_unit").setValue(pricePerUnit);
             } else {
-                const productRecord = await Xrm.WebApi.retrieveRecord("product", productId, "?$select=defaultpriceperunit");
+                const productRecord = await Xrm.WebApi.retrieveRecord("product", productId, "?$select=mon_price_per_unit");
                 Form.getAttribute("cr62c_mon_price_per_unit").setValue(productRecord.defaultpriceperunit);
             }
 
