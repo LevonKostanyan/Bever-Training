fa75384fcdfa96297441dda9e0c7666cd50d72ba
LevonKostanyan <levonkostanyan2021@mail.ru>
2024-09-14 23:34:37 +0400

    minor fix

--- price_list.js
+++ price_list.js
@@ -1,24 +1,26 @@
 async function initializePriceList(formContext) {
-    const priceListId = formContext.data.entity.getId();
+    const priceListId = formContext.data.entity.getId().replace('{', '').replace('}', '');
 
     try {
-        const priceListItems = await Xrm.WebApi.retrieveMultipleRecords("priceListItem", `?$filter=_pricelevelid_value eq ${priceListId}`);
+        const priceListItems = await Xrm.WebApi.retrieveMultipleRecords("cr62c_price_list_items", `?$filter=_cr62c_fk_price_list_value eq '${priceListId}'`);
+
         for (const item of priceListItems.entities) {
-            await Xrm.WebApi.deleteRecord("priceListItem", item.pricelistitemid);
+            await Xrm.WebApi.deleteRecord("cr62c_price_list_items", item.cr62c_price_list_itemsid);
         }
 
         const products = await Xrm.WebApi.retrieveMultipleRecords("product", "?$select=productid,name");
 
         for (const product of products.entities) {
             const priceListRecord = {
-                "pricelevelid@odata.bind": `/pricelevels(${priceListId})`,
-                "productid@odata.bind": `/products(${product.productid})`,
-                "uomid@odata.bind": "/uoms(00000000-0000-0000-0000-000000000000)",
-                "amount": 1, // Price Per Unit
-                "productdescription": product.name,
+                "cr62c_fk_price_list@odata.bind": `/cr62c_price_lists(${priceListId})`,
+                "cr62c_fk_product@odata.bind": `/products(${product.productid})`,
+                "uomid@odata.bind": "/uoms(19aa3f26-6f0b-4076-bc8f-9c773148ffbf)",
+                "cr62c_mon_price_per_unit": 1,
+                "cr62c_product_description": product.name,
                 "transactioncurrencyid@odata.bind": `/transactioncurrencies(${formContext.getAttribute("transactioncurrencyid").getValue()[0].id})`
             };
-            await Xrm.WebApi.createRecord("priceListItem", priceListRecord);
+
+            await Xrm.WebApi.createRecord("cr62c_price_list_items", priceListRecord);
         }
 
         Xrm.Navigation.openAlertDialog({ text: "Price List initialization completed successfully!" });
